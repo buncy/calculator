954633804829924c5d1592870b7a27b2516a824d
Your Name <53968167+buncy@users.noreply.github.com>
2019-10-21 16:52:16 +0530

    new changes-1

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-class calculator{
+class Calculator{
 constructor(previousOperandTextElement, currentOperandTextElement){
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
@@ -12,23 +12,82 @@ this.operation = undefined
 }
 
 delete(){
-
+this.currentOperand = this.currentOperand.toString().slice(0,-1)
 }
 
 appendNumber(number){
-
+    if(number=== '.' && this.currentOperand.includes('.')) return
+this.currentOperand = this.currentOperand.toString() + number.toString()
 }
 
 chooseOperation(operation){
-
+    if(this.currentOperand === '') return
+    if(this.previousOperand !== ''){
+        this.compute()
+    }
+this.operation = operation
+this.previousOperand = this.currentOperand
+this.currentOperand = ''
 }
 
 compute(){
+let computation
+const per = parseFloat(this.previousOperand)
+const current= parseFloat(this.currentOperand)
+if(isNaN(prev) || isNaN(current))return
+switch(this.operation){
+    case '+':
+        computation = prev + current
+        break
+    case '-':
+        computation = prev - current
+        break
+    case '*':
+        computation = prev * current
+        break
+    case '÷':
+        computation = prev / current
+        break
+    default :
+    return
+}
+this.currentOperand = computation
+this.operation = undefined
+this.previousOperand = ''
+}
 
+getDisplayNumber(number){
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)){
+        integerDisplay = ''
+
+    }
+    else{
+        integerDisplay = integerDigits.toLocaleString('en',{
+            maximumFractionDigits: 0
+        })
+    }
+    if (decimalDigits !=null){
+        return `${integerDisplay}.${decimalDigits}`
+    }else{
+        return integerDisplay
+    }
 }
 
 updateDisplay(){
-this.currentOperandTextElement.innerText
+this.currentOperandTextElement.innerText = this.currentOperand
+this.getDisplayNumber(this.currentOperand)
+if(this.operation != null){
+    this.previousOperandTextElement.innerText =
+    `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+
+
+}else{
+    this.previousOperandTextElement.innerText = ''
+}
 }
 
 
@@ -42,7 +101,7 @@ this.currentOperandTextElement.innerText
 
 }
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButton = document.querySelectorAll('[data-operation]')
+const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelectorAll('[data-allclear]');
@@ -50,9 +109,9 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 
-const calculator = new calculator(previousOperandTextElement,currentOperandTextElement){
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
 
-}
+
 numberButtons.forEach(button =>{
     button.addEventListener('click' , ()=>{
         calculator.appendNumber(button.innerText)
@@ -60,13 +119,27 @@ numberButtons.forEach(button =>{
     })
 })
 
+operationButtons.forEach(button =>{
+    button.addEventListener('click' , ()=>{
+        calculator.appendOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
 
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
 
-
-
-
-
+deleteButton.addEventListener('click', button =>{
+    calculator.delete()
+    calculator.updateDisplay()
+})
 
 
 
